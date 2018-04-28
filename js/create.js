5a670e0d93835713639a3f3c0e2d8b81e62b6058
=lihang <2655083706@qq.com>
2018-04-28 20:36:20 +0800

    提交

---
+++ js/create.js
@@ -0,0 +1,19 @@
+	
+$(document).ready(function(){ 
+		$.ajax({ 
+		type:"GET", 
+			url:"http://api.tianapi.com/mobile/?key=cf7f32ca14511e50603a087199789818&num=50",
+		    dataType:"json", 
+			success:function(data){ 
+				var txapi="<ul>";
+				for(var i=0;i<20;i++){
+					txapi+="<li class='content am-cf'><div class='news-img am-fl'><img src=" + data.newslist[i]["picUrl"] +"/></div><a class='news-content' href=" +data.newslist[i]["url"] + "><p class='news-content-title' >" + data.newslist[i]["title"] + "</p><p class='news-content-desc'><span>"+data.newslist[i]["description"] +"</span><span>" +data.newslist[i]["ctime"]+ "</span></p></a></li>";
+				}
+				txapi+="</ul>";
+				$('#result2').append(txapi); 
+				console.log(data.newslist);
+				} 
+
+			}); 
+			return false; 
+}); 
